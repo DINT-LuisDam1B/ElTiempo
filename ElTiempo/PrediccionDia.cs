83cdbf02392268a6a4e9289322900a6fba6e8c9b
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-11-20 12:24:44 +0100

    Commit actualización

--- ElTiempo/PrediccionDia.cs
+++ ElTiempo/PrediccionDia.cs
@@ -78,13 +78,13 @@ namespace ElTiempo {
         {
             ObservableCollection<PrediccionDia> resultado = new ObservableCollection<PrediccionDia>();
 
-            resultado.Add(new PrediccionDia("Lunes", "sol", 44, 60));
-            resultado.Add(new PrediccionDia("Martes", "nubes", 55, 70));
-            resultado.Add(new PrediccionDia("Miércoles", "sol", 35, 55));
-            resultado.Add(new PrediccionDia("Jueves", "nubes", 48, 65));
-            resultado.Add(new PrediccionDia("Viernes", "lluvia", 37, 50));
-            resultado.Add(new PrediccionDia("Sábado", "nubes", 30, 50));
-            resultado.Add(new PrediccionDia("Domingo", "nieve", 28, 40));
+            resultado.Add(new PrediccionDia("Lunes",        "sol",      44, 60));
+            resultado.Add(new PrediccionDia("Martes",       "nubes",    55, 70));
+            resultado.Add(new PrediccionDia("Miércoles",    "sol",      35, 55));
+            resultado.Add(new PrediccionDia("Jueves",       "nubes",    48, 65));
+            resultado.Add(new PrediccionDia("Viernes",      "lluvia",   37, 50));
+            resultado.Add(new PrediccionDia("Sábado",       "nubes",    30, 50));
+            resultado.Add(new PrediccionDia("Domingo",      "nieve",    28, 40));
 
             return resultado;
         }
